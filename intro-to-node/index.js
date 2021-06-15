0a7bab1ecf050fd7224cbf48e1f8636960086a1a
Samuel Mace <samuelcmace@gmail.com>
2021-06-15 14:45:03 -0500

    Section 18: Node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = superheroes.random();
+
+console.log("My super hero is " + mySuperHeroName);
+console.log("My super villain is " + mySuperVillainName);
